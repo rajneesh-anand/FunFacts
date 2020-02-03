eacbe8529626d16369384b1b9b88a2cda41821a0
=rajneesh.k.anand@gmail.com <=rajneesh.k.anand@gmail.com>
2020-02-03 13:05:53 +0530

    typo error

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const routes = require('./routes/routeHandler');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const routes = require("./routes/routeHandler");
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-
 var hbs = exphbs.create({
-    helpers: {
-        hello: function () { return Math.floor(Math.random() * Math.floor(10)); }
-    }
+	helpers: {
+		hello: function() {
+			return Math.floor(Math.random() * Math.floor(10));
+		},
+		domain_url: process.env.BASE_URL,
+		desc: "funny memes tredning tiktok videos",
+		title: "funny memes trending tiktok",
+		fb_id: process.env.FB_ID
+	}
 });
 
-// set view engine 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// set view engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set the static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Data parsing
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+	express.urlencoded({
+		extended: false
+	})
+);
 app.use(express.json());
 
 app.use(cors());
-app.use('/', routes);
+app.use("/", routes);
 
 // Server start
-app.listen(PORT, () => {console.log(`The server is running on port ${PORT}`)});
+app.listen(PORT, () => {
+	console.log(`The server is running on port ${PORT}`);
+});
