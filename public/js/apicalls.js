54c2352e56387bef5123b846c0b830d605b234bd
=rajneesh.k.anand@gmail.com <=rajneesh.k.anand@gmail.com>
2020-02-05 04:10:04 +0530

    typo

--- public/js/apicalls.js
+++ public/js/apicalls.js
@@ -1,7 +1,7 @@
 export default {
 	search: function(searchTerm, searchLimit, sortBy) {
 		return fetch(
-			`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+			`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
 		)
 			.then(res => res.json())
 			.then(data => {
